a26d373f3ac57bd5c9ebf494e9a59696b42ba09c
Dolagobinda Behera <dbehera@bridgetree.com>
2019-06-06 19:10:07 +0530

    Added new model

---
+++ ArtGalleryECommerce/ArtGalleryECommerce.UI/Models/ItemGroupModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArtGalleryECommerce.UI.Models
+{
+    public class ItemGroupModel
+    {
+        public int GroupId { get; set; }
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Group Name is Mandatory"), MaxLength(40)]
+        public string GroupName { get; set; }
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Group Desc is Mandatory"), MaxLength(400)]
+        public string GroupDesc { get; set; }
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Group Image is Mandatory"), MaxLength(500)] 
+        public string GroupImage { get; set; }
+        public HttpPostedFile ImageFile { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTimeOffset ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public int IsActive { get; set; }
+    }
+}
