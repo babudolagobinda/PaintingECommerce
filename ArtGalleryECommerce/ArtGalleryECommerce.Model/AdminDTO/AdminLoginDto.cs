571f0ec0ae3aa24144a335ab6e6f52a1fbe3117c
Dolagobinda Behera <dbehera@bridgetree.com>
2019-06-06 15:16:32 +0530

    Added two files

---
+++ ArtGalleryECommerce/ArtGalleryECommerce.Model/AdminDTO/AdminLoginDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtGalleryECommerce.Model.AdminDTO
+{
+    public class AdminLoginDto
+    { 
+        public int AdminId { get; set; }
+        public string Name { get; set; }
+        public string EmailId { get; set; }
+        public string ProfPic { get; set; }
+        public string UserName { get; set; }
+        public string UserRole { get; set; }
+        public string Password { get; set; }
+        public int IsActive { get; set; }
+    }
+}
---
+++ ArtGalleryECommerce/ArtGalleryECommerce.Model/AdminDTO/ItemGroupDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtGalleryECommerce.Model.AdminDTO
+{
+    public class ItemGroupDto
+    {
+        public int GroupId { get; set; }
+        public string GroupName { get; set; }
+        public string GroupDesc { get; set; }
+        public string GroupImage { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTimeOffset ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public int IsActive { get; set; }
+
+    }
+}
