2900879a370dbe6a0280c8c98252fcdafbaa2aaf
Dolagobinda Behera <dbehera@bridgetree.com>
2019-06-06 15:21:32 +0530

    Added Dal Files

---
+++ ArtGalleryECommerce/ArtGalleryECommerce.Dal/Admin/AdminLoginDal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArtGalleryECommerce.Model.AdminDTO;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ArtGalleryECommerce.Dal.Admin
+{
+    public class AdminLoginDal
+    {
+        SqlConnection con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["conn"].ConnectionString);
+        public AdminLoginDto GetAdminLogin(AdminLoginDto adminData)
+        {
+            AdminLoginDto adminLoginDto = new AdminLoginDto();
+            SqlCommand cmd = new SqlCommand("GetAdminDetails", con);
+            cmd.Parameters.AddWithValue("@UserName", adminData.UserName);
+            cmd.Parameters.AddWithValue("@Password", adminData.Password);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while(dr.Read())
+            {
+                adminLoginDto.AdminId = Convert.ToInt32(dr["AdminId"]);
+                adminLoginDto.Name = Convert.ToString(dr["Name"]);
+                adminLoginDto.EmailId = Convert.ToString(dr["EmailId"]);
+                adminLoginDto.ProfPic = Convert.ToString(dr["ProfPic"]);
+                adminLoginDto.UserName = Convert.ToString(dr["UserName"]);
+                adminLoginDto.Password = Convert.ToString(dr["Password"]);
+                adminLoginDto.UserRole = Convert.ToString(dr["UserRole"]);
+            }
+            return adminLoginDto;
+        }
+    }
+}
---
+++ ArtGalleryECommerce/ArtGalleryECommerce.Dal/Repository/AdminAuthenticationRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArtGalleryECommerce.Model.AdminDTO;
+using ArtGalleryECommerce.Dal.Admin;
+
+namespace ArtGalleryECommerce.Dal.Repository
+{
+   public class AdminAuthenticationRepository
+    {
+        public static AdminLoginDto GetAdminDetails(AdminLoginDto admin)
+        {
+            AdminLoginDal adminLoginDal = new AdminLoginDal();
+            AdminLoginDto adminDetails= adminLoginDal.GetAdminLogin(admin);
+            return adminDetails;
+        }
+        // static AdminLoginDto users = GetAdminDetails(AdminLoginDto admin);
+        //public static AdminLoginDto GetUserDetails(AdminLoginDto adminDetails)
+        //{
+        //    return users.Where(u => u.UserName.ToLower() == adminDetails.UserName.ToLower() &&
+        //    u.Password == adminDetails.Password).FirstOrDefault();
+        //}
+    }
+}
